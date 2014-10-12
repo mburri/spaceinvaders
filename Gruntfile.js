57096fe405f375fca497c8833199980c86c7f979
Max Burri <max.burri@gmail.com>
2014-10-12 10:07:32 +0200

    replaced grunt-serve with grunt-contrib-connect

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,43 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
-        serve: {
-            options: {
-                port: 9000
+
+        connect: {
+            server: {
+                options: {
+                    port: 9000,
+                    open: true,
+                    hostname: 'localhost'
+                    //    livereload: 35729,
+                    // Change this to '0.0.0.0' to access the server from outside
+                 
+                }
             }
-        }, 
+        },
+
         jshint: {
             all: ['*.js']
         },
         watch: {
             scripts: {
                 files: ['*.js'],
-                tasks: ['jshint']
+                tasks: ['jshint'],
+                options: {
+                    livereload: true
+                }
             }
         }
     });
 
-    grunt.loadNpmTasks('grunt-serve');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
 
-//grunt.registerTask('default', ['jshint', 'serve', 'watch']);
+    //grunt.registerTask('default', ['jshint', 'serve', 'watch']);
     grunt.registerTask('serve', function(target) {
-       grunt.task.run([
+        grunt.task.run([
             'jshint',
+            'connect',
             'watch'
         ]);
     });
