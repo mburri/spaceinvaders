a12a2209e87d0fe5d72e34e4023e96aca392fdab
Max Burri <max.burri@gmail.com>
2014-10-12 11:09:10 +0200

    draw the player

--- game.js
+++ game.js
@@ -1,1 +1,65 @@
-console.log("space invadeffffrs!");
+(function() {
+    'use strict';
+
+    var Game = function(canvasId) {
+        var canvas = document.getElementById(canvasId);
+        var screen = canvas.getContext('2d');
+        var gameSize = {
+            x: canvas.width,
+            y: canvas.height
+        };
+
+        this.bodies = [new Player(this, gameSize)];
+        var self = this;
+
+        var tick = function() {
+            self.update();
+            self.draw(screen, gameSize);
+
+            requestAnimationFrame(tick);
+        };
+
+        tick();
+    };
+
+    Game.prototype = {
+        update: function() {
+
+        },
+
+        draw: function(screen, gameSize) {
+            for (var i = this.bodies.length - 1; i >= 0; i--) {
+                drawRect(screen, this.bodies[i]);
+            }
+        }
+    };
+
+    var Player = function(game, gameSize) {
+        this.game = game;
+        this.size = {
+            x: 15,
+            y: 15
+        };
+        this.center = {
+            x: gameSize.x / 2,
+            y: gameSize.y - this.size.x
+        };
+    };
+
+    Player.prototype = {
+        update: function() {
+
+        }
+    };
+
+    var drawRect = function(screen, body) {
+        screen.fillRect(body.center.x - body.size.x / 2,
+            body.center.y - body.size.y / 2,
+            body.size.x, body.size.y);
+    };
+
+    window.onload = function() {
+        new Game("screen");
+    };
+
+})();
